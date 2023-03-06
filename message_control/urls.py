1a5726573c55e34ef70fa4b04b517be40f07e4ee
AntonyanNarek <Nairecks@mail.ru>
2023-03-06 17:20:26 +0300

    Представления для сообщений, правка в ссылках

---
+++ message_control/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import *
+from django.urls import path, include
+router = DefaultRouter(trailing_slash=False)
+
+router.register("message", MessageView)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
