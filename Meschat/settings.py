f290b855a6d87e66e88cd5ed4dcf3149e208543c
AntonyanNarek <Nairecks@mail.ru>
2023-05-30 19:45:39 +0300

    подключение swagger

--- Meschat/settings.py
+++ Meschat/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'user_control',
     'message_control',
+    'drf_yasg',
     'channels'
 ]
 
--- Meschat/urls.py
+++ Meschat/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user_control.urls')),
     path('message/', include('message_control.urls'))
 ]
+
+urlpatterns += doc_urls
