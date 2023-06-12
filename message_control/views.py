492029402fdadc0eaeb20d6137298cd9ce00febf
AntonyanNarek <Nairecks@mail.ru>
2023-06-12 16:27:44 +0300

    fix code

--- message_control/views.py
+++ message_control/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import *
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from Meschat.custom_methods import IsAuthenticatedCustom
 from rest_framework.response import Response
 from rest_framework import status
-from  django.db.models import Q
+from django.db.models import Q
 from django.conf import settings
 import requests
 import json
